113e1c788408dbedc2516f560e9bd8f7ab7509e9
Frans Benavides <138806696+jjfj2011@users.noreply.github.com>
2024-08-01 19:32:22 -0500

    Update grafico_barras_estados.py

--- grafico_barras_estados.py
+++ grafico_barras_estados.py
@@ -6,13 +6,16 @@ def crear_grafico(df):
         total_ventas_estados = ('valor_total','sum')
     ).sort_values(by='total_ventas_estados', ascending=True).reset_index()
 
+    # Formatear valores en dólares
+    revenue_estados['ventas'] = revenue_estados['total_ventas_estados'].apply(lambda x: f'${x:,.0f}')
+
     fig = px.bar(revenue_estados.tail(3),
                  x='state_name',
                  # x='total_ventas_estados',
                  y='total_ventas_estados',
                  # y='state_name',
-                 text = 'total_ventas_estados'#,
-                 #title = 'Top Ingresos por Estados ($)'
+                 text = 'total_ventas_estados',
+                 hover_data = {'ventas':True, 'total_ventas_estados':False}
                  )
     fig.update_layout(yaxis_title='Ventas ($)', xaxis_title='Estados', showlegend=False)
     fig.update_traces(texttemplate = '<b>%{text:.3s}</b>')
