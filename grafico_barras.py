d8c764bf38bafebea2a42f9b8505afa2a4d8054a
Frans Benavides <138806696+jjfj2011@users.noreply.github.com>
2024-08-01 19:14:11 -0500

    Update grafico_barras.py

--- grafico_barras.py
+++ grafico_barras.py
@@ -7,11 +7,15 @@ def crear_grafico(df):
         'valor_total':'sum'
     }).sort_values(by='valor_total', ascending=False).reset_index()
 
+    # Formatear valores en dólares
+    revenue_productos['ventas'] = revenue_productos['valor_total'].apply(lambda x: f'${x:,.0f}')
+
     fig = px.bar(revenue_productos.head(10),
                  x = 'tipo_producto',
                  y = 'valor_total',
                  text = 'valor_total',
-                 title = 'Top Ventas por Producto ($)'
+                 title = 'Top Ventas por Producto ($)',
+                 hover_data={'ventas':True, 'valor_total':False}
                  )
     
     fig.update_layout(yaxis_title='Ventas ($)', xaxis_title='Tipo de producto', showlegend=False, xaxis_tickangle=45)
